469f88100b70c3756660bdc790efac1097c08fc7
zack watkins <zackwatkins604@gmail.com>
2019-05-30 09:03:25 -0700

    Add Zack's constants file

---
+++ constants.py
@@ -0,0 +1,12 @@
+#for heat capacity c: https://theengineeringmindset.com/specific-heat-capacity-of-materials/
+#Energy Density
+"""Fuels•Natural gas ~ CH4 -- 56 MJ/kg
+Oil ~ CH2– 42 MJ/kg
+Coal ~ CH -- 15-33 MJ/kg
+Carbohydrates (food, CH2O) ~ 16 MJ/kg"""
+
+OilDen = 42
+NatGasDen = 56
+CoalDen = 20
+CarbsDen = 16
+GasDen = 34
